eb38ebedc8fbdaa196416b64dcd44ebefbcee294
Roman Larionov <roman.y.larionov@gmail.com>
2014-01-04 04:40:23 -0500

    bout to delete this branch.

--- js/functions.js
+++ js/functions.js
@@ -15,24 +15,19 @@ $(function() {
 
 	/*
 	===========================================================================
-		Section slide animation handler.
+		Segment slide animation handler.
 	*/
-
-
-
 
 	var webPage = $('html, body');
 
-	$("#navigation-dropdown a, .navbar-brand").bind("click",function(event){ 
-	 	// disables any animation from playing while active.
+	// Works for both the brand & dropdown links.
+	$("#navigation-dropdown a, .navbar-brand").bind("click",function(event) { 
+
 	 	event.preventDefault(); 
 
-	 	//stores the id of the desired part of the page into target.
 	 	var target = $(this).attr("href"); 
 
-	 	//stops any sort of animation that was occuring, then animates the page
-	 	//to scroll left and the the top of the section chosen. This animation
-	 	//lasts for 1.2 seconds.
+	 	// Animations for navbar buttons.
 	 	webPage.stop().animate({ 
 	 		scrollLeft: $(target).offset().left, 
 	 		scrollTop: $(target).offset().bottom }, 
@@ -44,30 +39,37 @@ $(function() {
 		Screen resize handler.
 	*/
 
-	// Optimization, stores reference to window outside event handler.
 	var $window = $(window);
 	var $wrapper = $(".wrapper");
 	var $segment = $(".segment");
 	var numTabs = $(".segment").length;
-	var $panel = $('#contact .panel');
 
 	function checkSize() {
-		//gets the current screen width;
+
+		// Getting various sizes.
 		var windowWidth = $window.width();
 		var windowHeight = $window.height();
 		var totalWidth = windowWidth * numTabs;
-		var panelHeight = $panel.height();
 
-		// document.write(panelHeight);
-		// $('.panel-body').height(panelHeight);
+		// If the window is too small, change the size of the message box.
+		$('.panel-body textarea').attr('rows', windowHeight/((1000/windowHeight)* 100));
 
-		// Algorithom for setting total wrapper width depending on window size.
+		// If window is too small, hide the info well.
+		if (windowHeight < 600) {
+			$('#contact .container > .well-sm').hide();
+		}
+
+		// For resize, to show well again.
+		else if (windowHeight >= 600) {
+			$('#contact .container > .well-sm').show();
+		}
+
+		// Algorithom for setting total wrapper size depending on window size.
 		$wrapper.width(totalWidth);
-		$wrapper.height(windowHeight);
+		$wrapper.height(windowHeight - 120);
 
-		// Algorithom for setting each segments width depending on total window size.
+		// Algorithom for setting each segments' width depending on total window size.
 		$segment.each(function() {
-			$(this).height(windowHeight - 150);
 			$(this).innerWidth(totalWidth/numTabs);
 		});
 	}
@@ -76,7 +78,6 @@ $(function() {
 	checkSize();
 	// Event listener for changing screen size.
 	$(window).resize(checkSize);
-
 
 	/*
 	===========================================================================
@@ -93,7 +94,8 @@ $(function() {
 
 	/*
 	===========================================================================
-		Code for switching the vertical scroll of the mouse to go horizontally.
+		Code for switching the vertical scroll of the mouse to horizontal.
+		!!!! Uses Mousewheel Library  /js/jquery.mousewheel.js !!!
 	*/
 
 	webPage.mousewheel(function(event, delta) {
