0811f06913f0cc02d51607fc944785ffa419143d
Roman Larionov <roman.y.larionov@gmail.com>
2014-01-04 05:27:20 -0500

    fixed contact page resize issue, brand button works, started home page.

--- js/functions.js
+++ js/functions.js
@@ -1,77 +1,106 @@
-$(document).ready(function() {
+$(function() {
 
 	/*
-	=========================================================================
-		Section slide animation handler.
+	===========================================================================
+		Code for dropdown color correction.
 	*/
 
-	$("#navigation-dropdown a").bind("click",function(event){ 
-	 	// disables any animation from playing while active.
+	$('.dropdownA, .A').hover(
+		function() {$('.dropdownA').css(
+			{'background-color': '#f2f2f2', 'color': '#000000'});},
+		function() {$('.dropdownA').css(
+			{'background-color': '#333', 'color': '#ffffff'});}
+	);
+
+
+	/*
+	===========================================================================
+		Segment slide animation handler.
+	*/
+
+	var webPage = $('html, body');
+
+	// Works for both the brand & dropdown links.
+	$("#navigation-dropdown a, .navbar-brand").bind("click",function(event) { 
+
 	 	event.preventDefault(); 
 
-	 	//stores the id of the desired part of the page into target.
 	 	var target = $(this).attr("href"); 
 
-	 	//stops any sort of animation that was occuring, then animates the page
-	 	//to scroll left and the the top of the section chosen. This animation
-	 	//lasts for 1.2 seconds.
-	 	$("html, body").stop().animate({ 
+	 	// Animations for navbar buttons.
+	 	webPage.stop().animate({ 
 	 		scrollLeft: $(target).offset().left, 
 	 		scrollTop: $(target).offset().bottom }, 
 	 		1200); 
 	});
 
 	/*
-	==================================================================
+	===========================================================================
 		Screen resize handler.
 	*/
 
-	// Optimization, stores reference to window outside event handler.
 	var $window = $(window);
 	var $wrapper = $(".wrapper");
 	var $segment = $(".segment");
 	var numTabs = $(".segment").length;
 
+	function checkSize() {
 
-	function checkWidth() {
-		//gets the current screen width;
-		var windowSize = $window.width();
-		var totalWidth = windowSize * numTabs;
+		// Getting various sizes.
+		var windowWidth = $window.width();
+		var windowHeight = $window.height();
+		var totalWidth = windowWidth * numTabs;
 
-		// Algorithom for setting total wrapper width depending on window size.
+		// If the window is too small, change the size of the message box.
+		$('.panel-body textarea').attr('rows', windowHeight/((1000/windowHeight)* 100));
+
+		// If window is too small, hide the info well.
+		if (windowHeight < 600) {
+			$('#contact .container > .well-sm').hide();
+		}
+
+		// For resize, to show well again.
+		else if (windowHeight >= 600) {
+			$('#contact .container > .well-sm').show();
+		}
+
+		// Algorithom for setting total wrapper size depending on window size.
 		$wrapper.width(totalWidth);
-		// Algorithom for setting each segments width depending on total window size.
+		$wrapper.height(windowHeight - 120);
+
+		// Algorithom for setting each segments' width depending on total window size.
 		$segment.each(function() {
 			$(this).innerWidth(totalWidth/numTabs);
 		});
 	}
 
 	// Executes on launch.
-	checkWidth();
+	checkSize();
 	// Event listener for changing screen size.
-	$(window).resize(checkWidth);
-<<<<<<< HEAD
-=======
+	$(window).resize(checkSize);
 
 	/*
 	===========================================================================
 		Code for keeping the scroll bar at current position.
 	*/
 
-
+	// $(window).resize(function() {
+	// 	webPage.mousewheel(function(event,delta) {
+	// 		this.scrollLeft === 0;
+	// 		event.preventDefault();
+	// 	});
+	// });
 
 
 	/*
 	===========================================================================
-		Code for switching the vertical scroll of the mouse to go horizontally.
+		Code for switching the vertical scroll of the mouse to horizontal.
+		!!!! Uses Mousewheel Library  /js/jquery.mousewheel.js !!!
 	*/
 
 	webPage.mousewheel(function(event, delta) {
 		this.scrollLeft -= (delta * 40);
 		event.preventDefault();
 	});
-	
-
->>>>>>> parent of 5af3597... added support for smaller window sizes.
 });
 
